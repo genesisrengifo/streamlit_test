68cce73bb45a054e9309c13027eef8ee2b61f1fe
genesisrengifo <genesisrengifov@gmail.com>
2025-08-06 17:09:12 -0400

    modificado

--- app.py
+++ app.py
@@ -1,39 +1,5 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "3757734c-c1a5-4ab5-b0fa-fd32ca56683e",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import streamlit as st\n",
-    "\n",
-    "st.header('Lanzar una moneda')\n",
-    "\n",
-    "st.write('Esta aplicación aún no es funcional. En construcción.')"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
