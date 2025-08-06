b848974949f9e8ed86af62b9749a9cf2dc40640c
genesisrengifo <genesisrengifov@gmail.com>
2025-08-06 16:46:37 -0400

    None en null

--- appy.py
+++ appy.py
@@ -2,20 +2,36 @@
  "cells": [
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": None,
    "id": "18ac2f05-6342-409c-bbfd-cfbdba45c557",
    "metadata": {},
    "outputs": [],
-   "source": []
+   "source": [
+    "import streamlit as st\n",
+    "\n",
+    "st.header('Lanzar una moneda')\n",
+    "\n",
+    "st.write('Esta aplicación aún no es funcional. En construcción.')"
+   ]
   }
  ],
  "metadata": {
   "kernelspec": {
-   "display_name": "",
-   "name": ""
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
   },
   "language_info": {
-   "name": ""
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.5"
   }
  },
  "nbformat": 4,
