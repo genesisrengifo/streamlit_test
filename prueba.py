6480a6f56a654903c7b405dd230b23d278d2e3ef
genesisrengifo <genesisrengifov@gmail.com>
2025-08-06 16:52:30 -0400

    nuevo archivo de prueba

---
+++ prueba.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "af2aa83b-e996-4ec7-a023-58be11d171b4",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
